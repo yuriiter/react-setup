00b38cdd8a625de6e1f5886742acbf7abb48e9e2
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2024-04-12 05:53:32 -0700

    configured redux with middleware to save state to local storage with debounce

---
+++ src/reduxMiddleware.ts
@@ -0,0 +1,27 @@
+import { Dispatch, Middleware, UnknownAction } from "redux";
+
+const SAVE_DELAY = 1000;
+
+export const saveToLocalStorage: Middleware<
+  object,
+  unknown,
+  Dispatch<UnknownAction>
+> = (store) => {
+  let timeout: ReturnType<typeof setTimeout> | null = null;
+
+  return (next) => (action) => {
+    const result = next(action);
+
+    if (timeout) clearTimeout(timeout);
+
+    timeout = setTimeout(
+      () =>
+        window.localStorage.setItem(
+          "appState",
+          JSON.stringify(store.getState()),
+        ),
+      SAVE_DELAY,
+    );
+    return result;
+  };
+};
--- src/store/rootStore.ts
+++ src/store/rootStore.ts
@@ -1,6 +1,15 @@
-import { createStore } from "redux";
 import rootReducer from "../reducers/rootReducer";
+import { configureStore } from "@reduxjs/toolkit";
+import { saveToLocalStorage } from "../reduxMiddleware";
 
-const store = createStore(rootReducer);
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().prepend(saveToLocalStorage),
+});
 
 export default store;
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppStore = typeof store;
